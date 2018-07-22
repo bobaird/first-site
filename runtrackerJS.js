3de7d0ba3ccd291ee141766e340d1095379a2258
bobaird <40519828+bobaird@users.noreply.github.com>
2018-07-22 19:57:28 -0400

    Add files via upload
    First commit of run tracker page, only offline

---
+++ runtrackerJS.js
@@ -0,0 +1,15 @@
+var time;
+//var distance = document.getElementByID("distance").value;
+var add = 4;
+console.log(5);
+
+function appendrow(){
+	var tb1 = document.getElementByID('my-table');
+	var row = tb1.insertRow((tb1.rows.length);
+	var i;
+	
+	for(i=0; i < tbl.rows[0].cells.length; i++) {
+		createCell(row.insertCell(i), i, 'row');
+    }
+}
+
